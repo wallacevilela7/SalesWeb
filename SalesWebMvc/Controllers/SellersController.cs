e298627cfc758fcf32e8c7c99bceb35b9807c112
Wallace Vilela <wallace.vilela9@gmail.com>
2023-05-01 20:11:09 -0300

    Simple Create form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,19 @@ namespace SalesWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list); //olha só o MVC ACONTECENDO DINAMICAMENTE
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
